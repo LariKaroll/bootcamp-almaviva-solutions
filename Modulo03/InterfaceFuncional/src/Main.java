6e5f64c0c9d1743ec3f9ca06ae5268f8d7c92621
larikaroll <larikarol108@gmail.com>
2026-04-12 12:45:16 -0300

    fix: update interfaceFuncional and exercicio01 sobre inteface terminado

--- Modulo03/InterfaceFuncional/src/Main.java
+++ Modulo03/InterfaceFuncional/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var user = List.of(new User("Larissa", 22),new User("Vitor", 27),new User("Gabriel", 29),new User("Kalel", 1),new User("Matheus", 56));
+
+        printStringValue(Record::toString,user);
+    }
+
+    public static void printStringValue(Function<User, String> callback, List<User> users){
+        users.forEach(u -> System.out.println(callback.apply(u)));
     }
 }
--- Modulo03/InterfaceFuncional/src/User.java
+++ Modulo03/InterfaceFuncional/src/User.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public record User() {
+public record User(String name, int age) {
 }
